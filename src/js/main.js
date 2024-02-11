c4a612b59e4e8a19b313eb58693d3760ca7d4a1c
Eric Thayer <eric.thayer594@gmail.com>
2024-02-11 09:39:14 -0700

    detecting sticky header event & styling

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,12 @@ import * as bootstrap from 'bootstrap'
 
 // BS color picker
 import './bs-color-picker.js'
+
+// Sticky header event detection
+const el = document.querySelector("#appHeader")
+const observer = new IntersectionObserver( 
+  ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
+  { threshold: [1] }
+);
+
+observer.observe(el);
