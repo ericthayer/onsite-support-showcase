35528e25e87e8ab5dfe70e33df9df4750d094425
Eric Thayer <eric.thayer594@gmail.com>
2024-02-11 23:49:47 -0700

    lightbox and template card styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,33 @@
 // Import our custom CSS
-import '../scss/styles.scss'
+import "../scss/styles.scss"
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap"
 
 // BS color picker
-import './bs-color-picker.js'
+import "./bs-color-picker.js"
+
+// BS Lightbox
+import Lightbox from "bs5-lightbox"
+
+const options = {
+  keyboard: true,
+  size: "xl",
+}
+
+document.querySelectorAll(".lightbox-toggle").forEach((el) =>
+  el.addEventListener("click", (e) => {
+    e.preventDefault()
+    const lightbox = new Lightbox(el, options)
+    lightbox.show()
+  })
+)
 
 // Sticky header event detection
 const el = document.querySelector("#appHeader")
-const observer = new IntersectionObserver( 
+const observer = new IntersectionObserver(
   ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
   { threshold: [1] }
-);
+)
 
-observer.observe(el);
+observer.observe(el)
